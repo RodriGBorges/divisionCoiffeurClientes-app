bc47c2307f8632c3842334befd14e14510e6cf96
Rodrigo Garayalde Borges <rodrii.gborges@gmail.com>
2024-01-27 19:12:14 -0300

    hash, token, login y logout

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,24 +1,72 @@
-import User from '../models/user.model.js'
+import User from '../models/user.model.js';
+import bcrypt from 'bcryptjs';
+import { crearTokenAcceso } from '../libs/jwt.js';
 
 export const register = async (req, res) => {
     const {email, contraseña, usuario } = req.body;
     
     try {
+
+        const contraseñaHash = await bcrypt.hashSync(contraseña, 12);
         const nuevoUsuario = new User({
             usuario,
             email,
-            contraseña
+            contraseña: contraseñaHash
         });
-    
+        
         const usuarioGuardado = await nuevoUsuario.save();
-        res.json(usuarioGuardado);
-
+        const token = await crearTokenAcceso({id: usuarioGuardado._id})
+        
+        res.cookie('token', token);
+        res.json({
+            id:usuarioGuardado._id,
+            usuario: usuarioGuardado.usuario,
+            email: usuarioGuardado.email,
+            createdAt: usuarioGuardado.createdAt,
+            updatedAt: usuarioGuardado.updatedAt,
+        });
     } catch (error) {
         console.log(error);
+        return res.status(500).json({ message: error.message });
     }
 
 }
 
-export const login = (req, res) => {
-    res.send('login');
+export const login = async (req, res) => {
+        const {email, contraseña } = req.body;
+        
+        try {
+            const usuarioEncontrado = await User.findOne({email});
+
+            if(!usuarioEncontrado) {
+                return res.status(400).json({ message: "Usuario no encontrado"})
+            };
+
+            const clavesIguales = await bcrypt.compareSync(contraseña, usuarioEncontrado.contraseña);
+
+            if(!clavesIguales) {
+                return res.status(400).json({ message: "Usuario o contraseña incorrecto"})
+            }
+            
+            const token = await crearTokenAcceso({id: usuarioEncontrado._id})
+            
+            res.cookie('token', token);
+            res.json({
+                id:usuarioEncontrado._id,
+                usuario: usuarioEncontrado.usuario,
+                email: usuarioEncontrado.email,
+                createdAt: usuarioEncontrado.createdAt,
+                updatedAt: usuarioEncontrado.updatedAt,
+            });
+        } catch (error) {
+            console.log(error);
+            return res.status(500).json({ message: error.message });
+        }
+}
+
+export const logout = (req, res) => {
+    res.cookie('token', '', {
+        expires: new Date(0)
+    });
+    return res.sendStatus(200);
 }
